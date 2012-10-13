375639ba425709bc269be8c5a76d891138567431
Ammar Askar <ammar_askar@hotmail.com>
2012-10-13 22:15:45 +0500

    Initial commit

---
+++ src/com/ammaraskar/bukkit/CommandItem.java
@@ -0,0 +1,82 @@
+package com.ammaraskar.bukkit;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CommandItem extends JavaPlugin implements Listener, CommandExecutor {
+    
+    static String DEFAULT_MESSAGE = ChatColor.RED + "You need to be holding %item% to use that command";
+    Map<String, String> commands = new HashMap<String, String>(); // Maps command -> command message
+    
+    public void onEnable() {
+        saveDefaultConfig();
+
+        getCommand("commanditem").setExecutor(this);
+        getServer().getPluginManager().registerEvents(this, this);
+        
+        loadConfig(null);
+    }
+
+    @EventHandler(ignoreCancelled = true)
+    public void onCommandPreProcess(PlayerCommandPreprocessEvent event) {
+        getLogger().info("Processing " + event.getPlayer().getName() + "'s command of:");
+        String[] args = event.getMessage().substring(1, event.getMessage().length()).split(" ");
+        getLogger().info(Arrays.deepToString(args));
+
+        if (args.length > 0) {
+            if ((getConfig().contains("commands." + args[0])) && (event.getPlayer().getItemInHand().getTypeId() != getConfig().getInt("commands." + args[0]))) {
+                if (commands.get(args[0]) != null) {
+                    event.getPlayer().sendMessage(formatString(commands.get(args[0]), getConfig().getInt(new StringBuilder("commands.") + args[0])));
+                } else {
+                    event.getPlayer().sendMessage(formatString(DEFAULT_MESSAGE, getConfig().getInt(new StringBuilder("commands.") + args[0])));
+                }
+                event.setCancelled(true);
+            }
+        }
+    }
+    
+    public String formatString (String string, int id) {
+        return ChatColor.translateAlternateColorCodes('&', string.replace("%item%", Material.getMaterial(id).toString().toLowerCase().replace("_", " ")));
+    }
+    
+    public void loadConfig(CommandSender sender) {
+        for (String key : getConfig().getKeys(true)) {
+            if (key.startsWith("commands.")) {
+                if (Material.getMaterial(getConfig().getInt(key)) == null || key.length() < 9) {
+                    if (sender != null) {
+                        sender.sendMessage("Invalid id or key at '" + key + "' value of '" + getConfig().getInt(key) + "'");
+                    }
+                    getLogger().warning("Invalid id or key at '" + key + "' value of '" + getConfig().getInt(key) + "'");
+                }
+                String command = key.substring(9);
+                if (getConfig().getString("messages." + command) != null) { 
+                    commands.put(command, getConfig().getString("messages." + command));
+                } else {
+                    commands.put(command, null);
+                }
+                continue;
+            }
+        }
+    }
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if ((args.length == 1) && (args[0].equalsIgnoreCase("reload") && sender.hasPermission("commanditem.admin"))) {
+            this.reloadConfig();
+            this.loadConfig(sender);
+            sender.sendMessage(ChatColor.AQUA + "Reloaded!");
+            return true;
+        }
+        return false;
+    }
+}
